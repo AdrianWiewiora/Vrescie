37f5f9c268c0861495adb9ac29af416a50fad5ae
Adrian Wiewióra <93733010+AdrianWiewiora@users.noreply.github.com>
2024-01-05 12:19:58 +0100

    Refactor Add name, age and gender and preparation to profile fragment

--- VrescieAndroid/app/src/main/java/com/example/vrescieandroid/fragments/AddNameFragment.kt
+++ VrescieAndroid/app/src/main/java/com/example/vrescieandroid/fragments/AddNameFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.RadioButton
 import android.widget.RadioGroup
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
@@ -35,6 +37,19 @@ class AddNameFragment : Fragment() {
         nameEditText = view.findViewById(R.id.name)
         genderRadioGroup = view.findViewById(R.id.genderRadioGroup)
 
+        // W onCreateView() lub gdziekolwiek chcesz to zainicjować
+        val ageSpinner: Spinner = view.findViewById(R.id.ageSpinner)
+
+        // Przygotuj listę wieku od 16 do 120
+        val ages = (16..120).map { it.toString() }
+
+        // Utwórz adapter dla Spinner
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, ages)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        // Przypisz adapter do Spinner
+        ageSpinner.adapter = adapter
+
         val buttonNext = view.findViewById<Button>(R.id.nextBtn)
         buttonNext.setOnClickListener {
             val name = nameEditText.text.toString().trim()
@@ -44,14 +59,15 @@ class AddNameFragment : Fragment() {
                 Toast.makeText(requireContext(), "Wypełnij wszystkie pola", Toast.LENGTH_SHORT).show()
             } else {
                 val selectedGender = if (selectedGenderId == R.id.radioButtonFemale) "F" else "M"
-                saveUserDataToDatabase(name, selectedGender)
+                val selectedAge = ageSpinner.selectedItem.toString()
+                saveUserDataToDatabase(name, selectedGender, selectedAge)
             }
         }
 
         return view
     }
 
-    private fun saveUserDataToDatabase(name: String, gender: String) {
+    private fun saveUserDataToDatabase(name: String, gender: String, age: String) {
         val user: FirebaseUser? = auth.currentUser
 
         // Sprawdź, czy użytkownik jest zalogowany
@@ -63,7 +79,8 @@ class AddNameFragment : Fragment() {
             reference.updateChildren(
                 mapOf(
                     "name" to name,
-                    "gender" to gender
+                    "gender" to gender,
+                    "age" to age
                 )
             ).addOnCompleteListener { task ->
                 if (task.isSuccessful) {
