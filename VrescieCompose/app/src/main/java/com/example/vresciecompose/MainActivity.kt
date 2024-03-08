8ce4757cbf0fa8e3ae70f0849c96429bf0ac1902
Adrian Wiewióra <93733010+AdrianWiewiora@users.noreply.github.com>
2024-03-08 11:52:06 +0100

    New system splash screen

--- VrescieCompose/app/src/main/java/com/example/vresciecompose/MainActivity.kt
+++ VrescieCompose/app/src/main/java/com/example/vresciecompose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.vresciecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,10 +11,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.vresciecompose.components.MainViewModel
 import com.example.vresciecompose.ui.theme.VrescieComposeTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                !viewModel.isReady.value
+            }
+        }
+
         super.onCreate(savedInstanceState)
         setContent {
             VrescieComposeTheme {
