3fbaef0967e0d1bd7213c11f1c082419ca9404cb
Adrian Wiewióra <93733010+AdrianWiewiora@users.noreply.github.com>
2024-04-08 16:39:56 +0200

    Login with email ver 2

--- VrescieCompose/app/src/main/java/com/example/vresciecompose/view_models/LoginViewModel.kt
+++ VrescieCompose/app/src/main/java/com/example/vresciecompose/view_models/LoginViewModel.kt
@@ -1,8 +1,12 @@
 package com.example.vresciecompose.view_models
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -16,8 +20,23 @@ class LoginViewModel : ViewModel() {
                 if (task.isSuccessful) {
                     onSuccess()
                 } else {
-                    onFailure(task.exception?.message ?: "Unknown error")
+                    val errorCode = (task.exception as? FirebaseAuthException)?.errorCode
+                    Log.e("LoginViewModel", "Error code: $errorCode")
+                    val errorMessage = when (errorCode) {
+                        "ERROR_INVALID_CREDENTIAL" -> "Niepoprawne dane"
+                        "ERROR_INVALID_EMAIL" -> "Nieprawidłowy format adresu email"
+                        else -> {
+                            "Wystąpił nieznany błąd"
+                        }
+                    }
+                    Log.e("LoginViewModel", "Error message: $errorMessage")
+                    onFailure(errorMessage)
                 }
             }
     }
+
+
 }
+
+
+
